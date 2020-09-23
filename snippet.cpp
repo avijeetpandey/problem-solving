36748b9539616acd060f95b784b2f66311a731e5
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-09-23 12:00:07 +0530

    added cp snippet file

---
+++ snippet.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+
+void redirectIO(){
+	fast
+	#ifndef ONLINE_JUDGE
+		freopen("in.txt","r",stdin);
+		freopen("out.txt","w",stdout);
+	#endif
+}
+
+int main(){
+	redirectIO();
+	
+    
+
+	return 0;
+}
