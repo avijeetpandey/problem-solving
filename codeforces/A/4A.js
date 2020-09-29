ba12afd64b311184f61da2475c03b5b48f04ea36
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-09-29 23:54:19 +0530

    codeforces js try

--- codeforces/A/4A.js
+++ codeforces/A/4A.js
@@ -1,14 +1,13 @@
-'use strict';
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
-
+ 
 let inputString = '';
 let currentLine = 0;
-
+ 
 process.stdin.on('data', inputStdin => {
     inputString += inputStdin;
 });
-
+ 
 process.stdin.on('end', _ => {
     inputString = inputString.trim().split('\n').map(string => {
         return string.trim();
@@ -16,16 +15,13 @@ process.stdin.on('end', _ => {
     
     main();    
 });
-
+ 
 const readLine=()=> inputString[currentLine++];
-
-
+ 
+ 
 const main=()=>{
-	let t = parseInt(readLine())
-	while(t--){
-		let s = readLine()
-		if(s.length <= 10)
-			console.log(s)
-		else console.log(s[0]+(s.length-2)+s[s.length-1])
-	}
+	let n = parseInt(readLine())
+	if(n%2==0 && n!=2)
+		console.log("YES")
+	else console.log("NO")
 }
