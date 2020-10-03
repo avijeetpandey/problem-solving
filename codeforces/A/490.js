91b5868f05db1cf14a0ce561c18dfedd63b979bc
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-10-03 19:36:14 +0530

    codeforces sheet

--- codeforces/A/490.js
+++ codeforces/A/490.js
@@ -23,5 +23,24 @@ const readLine=()=> inputString[currentLine++];
 ===================================================*/
 
 const main=()=>{
-	
+	let n=+readLine()
+	let one = 0,two=0,three=0
+	let o=[],t=[],th=[]
+	let ls = readLine().split(" ").map((n,index)=>{
+		if(n==='1')
+			one++ , o.push(index+1)
+		else if (n==='2')
+			two++ , t.push(index+1)
+		else if(n==='3') three++, th.push(index+1)
+	return +n
+	})
+	let teams = Math.min(one,Math.min(two,three))
+	if(teams){
+		console.log(teams)
+		for(let i=0;i<teams;i++){
+			console.log(o[i],t[i],th[i])
+		}
+	}else{	
+		console.log(teams)
+	}
 }
