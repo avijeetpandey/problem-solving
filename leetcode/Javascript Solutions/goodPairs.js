b2913ad1b3b59cdbf9e29b03ccb339a46732c459
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-01-17 17:20:48 +0530

    add : leetcode Q-1512

---
+++ leetcode/Javascript Solutions/goodPairs.js
@@ -0,0 +1,10 @@
+const numIdenticalPairs = (nums)=>{
+    let count = 0
+    for(let i=0;i<nums.length-1;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]==nums[j])
+                count++
+        }
+    }
+    return count
+}
