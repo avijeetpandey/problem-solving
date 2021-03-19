dad2ba9706119b39dcb8050f00af7927d9eaf629
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-03-19 21:32:57 +0530

    Remove : Other languages files

--- leetcode/Python Solutions/runningSum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        runningSum = []
-        sum = 0
-        for i in range(0, len(nums)):
-            sum += nums[i]
-            runningSum.append(sum)
-        return runningSum
