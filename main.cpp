9b326a7eb04504da9b9e69af1503ef1b3132b2ac
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2021-06-19 20:52:33 +0000

    Add : Cpp sample I/O check

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include<bits/stdc++.h>
-#define int long long
-#define endl '\n'
-#define maxn 1000000
-#define fast_io ios_base::sync_with_stdio(NULL); cin.tie(NULL);
 using namespace std;
 
-void solve(){
-    fast_io
-    cout<<"Hello"<<endl;
-}
- 
-signed main(){
-    solve();
-    return 0;
+int main(){
+  string name;
+  cout<<"Enter the name : ";
+  cin>>name;
+
+  cout<<"Name is : "<<name<<endl;
+
+  return 0;
 }
