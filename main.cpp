e8fda498e1f88db1ea837b7bfc4c474c059cadc8
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2021-06-25 06:29:39 +0000

    Add : Basic Linked List

--- main.cpp
+++ main.cpp
@@ -1,12 +1,38 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
+class Node {
+  public:
+    int data;
+    Node* next;
+};
+
+void printList(Node* head){
+  Node* curr = head;
+  while(curr!=NULL){
+      cout<<curr->data<<" -> ";
+       curr = curr->next;
+  }
+  cout<<endl;
+}
+
 int main(){
-  string name;
-  cout<<"Enter the name : ";
-  cin>>name;
+  Node* head = NULL;
+  Node* second = NULL;
+  Node* third = NULL;
 
-  cout<<"Name is : "<<name<<endl;
+  head = new Node();
+  second = new Node();
+  third = new Node();
 
-  return 0;
+  head->data = 10;
+  second->data = 20;
+  third->data = 30;
+
+  head->next = second;
+  second->next = third;
+
+  printList(head);
+
+  return 0; 
 }
