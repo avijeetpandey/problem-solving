52a9dc43d37b9322a86835618e589f99e771b707
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-07-05 00:44:09 +0530

    Add : Hashmap demo

--- src/CollectionsDemo/HashMapDemo.java
+++ src/CollectionsDemo/HashMapDemo.java
@@ -8,24 +8,24 @@ import java.util.Map;
 
 public class HashMapDemo {
     public static void main(String[] args) {
-        HashMap<Integer,String> map = new HashMap<>();
+        HashMap<Integer, String> map = new HashMap<>();
 
         // array containing the numbers
-        ArrayList<String> numbers = new ArrayList<>(List.of("One","Two","Three","Four","Five","Six","Seven","Eight"));
+        ArrayList<String> numbers = new ArrayList<>(List.of("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight"));
 
         // inserting into hashmap
-        for(int i=0;i<numbers.size();i++){
-            map.put(i,numbers.get(i));
+        for (int i = 0; i < numbers.size(); i++) {
+            map.put(i, numbers.get(i));
         }
 
         // traversing the hashmap
-        for(Map.Entry<Integer,String> entry: map.entrySet()){
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
             Integer key = entry.getKey();
             String value = entry.getValue();
-            System.out.println("The Key and values are : "+key+" "+value);
+            System.out.println("The Key and values are : " + key + " " + value);
         }
 
         // size of the hashmap is
-        System.out.println("The size of the hashmap is : "+map.size());
+        System.out.println("The size of the hashmap is : " + map.size());
     }
 }
