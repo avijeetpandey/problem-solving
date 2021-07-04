33da5e74a57aa60bd48212dcd0e0646c107d1e06
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-07-04 23:47:58 +0530

    Add : 4A codeforces

--- src/com/avijeet/Main.java
+++ src/com/avijeet/Main.java
@@ -1,10 +1,19 @@
 package com.avijeet;
 
-import CollectionsDemo.ArrayListDemo;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
 
+        if (number % 2 == 0 && number > 2) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        scan.close();
     }
 }
