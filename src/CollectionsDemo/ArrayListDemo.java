b9d4037915ed7035f570e4157b6b16edcfcb405a
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-07-04 23:34:58 +0530

    Formatted : ArrayListDemo

--- src/CollectionsDemo/ArrayListDemo.java
+++ src/CollectionsDemo/ArrayListDemo.java
@@ -7,21 +7,21 @@ public class ArrayListDemo {
     public static void demo() {
 
         ArrayList<String> al = new ArrayList<>();
-        for(int i=0; i<10; i++){
+        for (int i = 0; i < 10; i++) {
             al.add(String.valueOf(i));
         }
 
         //  iterating over a array list
-        for(String s : al){
+        for (String s : al) {
             System.out.println(s);
         }
 
         // second way to iterate over array list
-        for(int i=0;i<al.size();i++){
-            System.out.println("The Element is "+al.get(i));
+        for (int i = 0; i < al.size(); i++) {
+            System.out.println("The Element is " + al.get(i));
         }
 
-        System.out.println("the size of list is : "+al.size());
+        System.out.println("the size of list is : " + al.size());
 
     }
 }
