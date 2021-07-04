d45f54f1c544f167324fbe230d875300c571dec6
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-07-05 00:06:46 +0530

    Add : 71A codeforces

--- src/com/avijeet/Main.java
+++ src/com/avijeet/Main.java
@@ -6,13 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-
-        if (number % 2 == 0 && number > 2) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
 
         scan.close();
     }
