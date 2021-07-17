a3d0919f17acb258b8a66db26e6a86ba20fb1e2b
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-07-17 17:13:34 +0530

    Add : Stack in swift

--- Data structures/Stack.swift
+++ Data structures/Stack.swift
@@ -1,35 +1,39 @@
-// Stack implementation in swift
+/*
+Stack implementation in swift
+STACK -> Has LIFO structure , ie Last In first out , this is a generic Stack
+*/
 
 public struct Stack<T> {
   fileprivate var array = [T]()
-  
-  public var isEmpty : Bool {
-      return array.isEmpty
+
+  // properties to get values
+  var isEmpty: Bool {
+    return array.isEmpty
   }
 
-  public var count : Int {
+  var count: Int {
     return array.count
   }
 
-  public var top : T? {
+  var top: T? {
     return array.last
   }
 
-  public mutating func pop() -> T? {
-    return array.popLast()
-  }
-
-  public mutating func push(_ element : T) {
+  // function to push an element to stack
+  public mutating func push(_ element: T) {
     array.append(element)
   }
 
+  // function to pop elements from stack
+  public mutating func pop() -> T? {
+    return array.popLast()
+  }
 }
 
-var stack  = Stack<Int>()
-stack.push(23)
-stack.push(34)
-print("The Size of stack is \(stack.count)")
+var stack = Stack<String>()
+stack.push("Hello")
+stack.push("Stack")
+stack.push("Swift")
+print("The size of the stack is \(stack.count)")
 stack.pop()
-print("The top element in stack is \(stack.top ?? 0)")
-stack.pop()
-print("The element in stack is : \(stack.count)")
+print("Is the stack empty : \(stack.isEmpty)")
