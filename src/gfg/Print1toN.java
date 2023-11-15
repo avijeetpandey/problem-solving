bfc0d432b17b96f65da2d068582e80ea45f62f94
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-11-15 21:45:07 +0530

    Add: program to print 1 to n recursively

---
+++ src/gfg/Print1toN.java
@@ -0,0 +1,15 @@
+package gfg;
+
+public class Print1toN {
+    static void print(int n) {
+        if(n==0){
+            return;
+        }
+        print(n-1);
+        System.out.println(n);
+    }
+
+    public static void main(String[] args){
+        print(5);
+    }
+}
