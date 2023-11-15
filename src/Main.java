f7f39b35178f687335d761403b1697029d42d05f
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-11-15 21:43:06 +0530

    Add: recursion problems

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-       scan.close();
+        scan.close();
     }
 }
---
+++ src/gfg/PrintNto1.java
@@ -0,0 +1,13 @@
+package gfg;
+
+public class PrintNto1 {
+    static void print(int n) {
+        if(n==0)
+            return;
+        System.out.println(n);
+        print(n-1);
+    }
+    public static void main(String[] args) {
+        print(10);
+    }
+}
