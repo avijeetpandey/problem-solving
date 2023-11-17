16ad51d92d3d3f14dcfa0e88a72dd711c91b5037
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-11-17 19:59:18 +0530

    Add: Reversal of linked list

--- src/leetcode/ReverseLinkedList.java
+++ src/leetcode/ReverseLinkedList.java
@@ -1,4 +1,36 @@
 package leetcode;
 
+// Program to reverse a linkedlist iteratively
 public class ReverseLinkedList {
+    public ListNode reverseLinkedList(ListNode node) {
+        ListNode current = node;
+        ListNode prev = null;
+        ListNode next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
