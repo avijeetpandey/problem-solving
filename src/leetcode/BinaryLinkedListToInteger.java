65f1b3df5fa4bfea73b77a3a4af326ee393771cb
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-11-19 11:48:04 +0530

    Fix : BinaryLinkedListToInteger class

--- src/leetcode/BinaryLinkedListToInteger.java
+++ src/leetcode/BinaryLinkedListToInteger.java
@@ -3,15 +3,15 @@ package leetcode;
 class BinaryLinkedListToInteger {
     public int getDecimalValue(ListNode head) {
         ListNode currentNode = head;
-        String list = "";
+        StringBuilder list = new StringBuilder();
         while (currentNode != null) {
-            list += "" + currentNode.val;
+            list.append(currentNode.val);
             currentNode = currentNode.next;
         }
-        return Integer.parseInt(list, 2);
+        return Integer.parseInt(list.toString(), 2);
     }
 
-    class ListNode {
+    static class ListNode {
         int val;
         ListNode next;
 
