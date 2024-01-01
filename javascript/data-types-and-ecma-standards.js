013fc0e139a6196ca383499ec808b25b49b169d8
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-01 16:39:05 +0530

    Add: ECMA standard notes

---
+++ javascript/data-types-and-ecma-standards.js
@@ -0,0 +1,18 @@
+"use strict";
+// By using strict treat all JS code as a newer version
+
+// ECMA defines the standards for writting javascript
+
+/**
+ * Datatypes
+ *  - number - 2^53
+ *  - bigint
+ *  - string
+ *  - boolean - true/false
+ *  - null - null is a standalone value in javascript
+ *  - undefined - no value has been assinged to variable till now
+ *  - symbol - used for defining uniquiness
+ */
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
