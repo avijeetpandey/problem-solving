90a0c210c1193a11db92362e73f5263f2c91e17e
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-01 16:50:02 +0530

    Add: ECMA standards

--- javascript/data-types-and-ecma-standards.js
+++ javascript/data-types-and-ecma-standards.js
@@ -16,3 +16,30 @@
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+let age = 33;
+
+console.log(typeof age);
+
+const value = "33abx";
+const valueInNumber = Number(value);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// NaN - is a type of number in javascript
+
+const isLoggedIn = null;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/**
+ * Boolean conversions
+ * 1 => true, 0 => false
+ * "" - false
+ * "not empty string" - true
+ * undefined - false
+ * null - false
+ */
