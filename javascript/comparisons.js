e998669ab2d03f387a7f0622e29621cf02f39db8
Avijeet pandey <avijeetpandey25@gmail.com>
2024-01-28 13:02:10 +0530

    Remove: all code

--- javascript/comparisons.js
+++
@@ -1,9 +0,0 @@
-console.log(2 > 1); //  always gives boolean values
-
-console.log("02" > 1); // true
-
-console.log(null > 0); // false
-console.log(null == 0); // false
-console.log(null >= 0); // true
-
-// The reason is that an equality check == and comparisons works differently comparisons convert null to number treating it as 0
--- leetcode/variableContains.js
+++
@@ -1,8 +0,0 @@
-const arr = ["abc","bcd","aaaa","cbc"]
-const x = "e";
-
-arr.forEach((el, index) => {
-  if (el.includes(x)) {
-    console.log({ el, x, index });
-  }
-});
