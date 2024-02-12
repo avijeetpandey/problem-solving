c26ba4f5e3885cd93001126d850fe1213e58b79d
Avijeet pandey <avijeetpandey25@gmail.com>
2024-02-13 01:49:50 +0530

    Add: Vector demo

---
+++ stl/vector.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // declaring vector , this line creates an empty container
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+
+    // defining vector of a size prefilled with elements
+    vector<int> prefilledVector(5, 100); // size of 5 filled with 100
+
+    // defining vector of a size
+    vector<int> prefilledWithZeroOrGrabageValue(5);
+
+    // copying a vector
+    vector<int> copyVector(v);
+
+    // iterating over a vector
+    for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " ";
+    }
+
+    // another way of iterating vectors
+    cout << endl
+         << "==========Second Method========" << endl;
+    for (auto it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " ";
+    }
+
+    return 0;
+}
