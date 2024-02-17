d925c79298f5cdbe1abd2570afad1b98b47d2fdb
Avijeet pandey <avijeetpandey25@gmail.com>
2024-02-17 22:10:25 +0530

    Add: union of arrays

---
+++ problems/unionOfArrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set<int> st;
+    vector<int> o = {1, 2, 3, 4, 5, 6};
+    vector<int> t = {1, 2, 3};
+
+    for (auto it : o)
+        st.insert(it);
+    for (auto it : t)
+        st.insert(it);
+
+    cout << st.size() << endl;
+
+    return 0;
+}
