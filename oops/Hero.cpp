c116e45b21b0671a8b83da1ecd4e224c19349f78
Avijeet pandey <avijeetpandey25@gmail.com>
2024-02-17 15:47:47 +0530

    Add: copy constructor

--- oops/Hero.cpp
+++ oops/Hero.cpp
@@ -5,10 +5,48 @@ class Hero
 {
 public:
     int health;
-    char name[100];
+    char *name;
 
     Hero()
     {
         cout << "Constructor called" << endl;
+        name = new char[100];
+    }
+
+    // parameterised constructor
+    Hero(int health)
+    {
+        // printing the address of the current object
+        cout << "this-> " << this << endl;
+        this->health = health;
+        cout << "Parameterised constructor called" << endl;
+    }
+
+    // custom implementation of the copy constructor
+    Hero(Hero &h)
+    {
+        this->health = h.health;
+        this->name = h.name;
+    }
+
+    // getters and setters
+    char *getName()
+    {
+        return this->name;
+    }
+
+    int getHealth()
+    {
+        return this->health;
+    }
+
+    void setHealth(int health)
+    {
+        this->health = health;
+    }
+
+    void setName(char name[])
+    {
+        strcpy(this->name, name);
     }
 };
--- oops/main.cpp
+++ oops/main.cpp
@@ -10,5 +10,17 @@ int main()
     // dynamic creation of class
     Hero *h = new Hero;
 
+    // parameterised call
+    Hero pHero(12);
+    char name[7] = "Aman";
+    pHero.setName(name);
+
+    // printing the address of the pHero
+    cout << "Address of pHero is: " << &pHero << endl;
+
+    Hero t(pHero);
+
+    cout << t.getName() << endl;
+
     return 0;
 }
