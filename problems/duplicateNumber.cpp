5c636528303fa56ca49bb55dbf22a40a23d4db57
Avijeet pandey <avijeetpandey25@gmail.com>
2024-02-17 19:03:27 +0530

    Add: duplicate elements

--- problems/duplicateNumber.cpp
+++ problems/duplicateNumber.cpp
@@ -1,50 +1,37 @@
 #include <iostream>
 #include <map>
 #include <vector>
-
 using namespace std;
 
-int findDuplicateNumber(vector<int> &nums)
+int findDuplicates(vector<int> &v)
 {
-    map<int, int> m;
-    for (auto it : nums)
+    map<int, int> mp;
+
+    for (auto it : v)
     {
-        if (m.find(it) == m.end())
+        if (mp.find(it) == mp.end())
         {
-            m[it] = 1;
+            mp[it] = 1;
         }
         else
         {
-            m[it]++; 
+            mp[it]++;
         }
     }
 
-    for (auto it : m)
+    for (auto it : mp)
     {
         if (it.second > 1)
         {
             return it.first;
         }
     }
-
-    return 0;
 }
 
 int main()
 {
     vector<int> v = {1, 3, 4, 2, 2};
-    int number = findDuplicateNumber(v);
-
-    // second method
-    sort(v.begin(), v.end());
-
-    for (int i = 0; i < v.size(); i++)
-    {
-        if (v[i] == v[i + 1])
-        {
-            cout << v[i] << endl;
-        }
-    }
-
+    int res = findDuplicates(v);
+    cout << res << endl;
     return 0;
 }
