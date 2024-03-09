7069967088e359379360d43b6d6e6fe9352c3f58
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-09 23:36:15 +0530

    Add: throwing function example

---
+++ problem-solving-swift/swift/ErrorHandling.swift
@@ -0,0 +1,57 @@
+//
+//  ErrorHandling.swift
+//  problem-solving-swift
+//
+//  Created by Avijeet on 09/03/24.
+//
+
+import Foundation
+
+// MARK: - Error
+enum VendingMachineError: Error {
+    case invalidSelection
+    case insufficientFunds(coinsNeeded: Int)
+    case outOfStock
+}
+
+struct Item {
+    var price: Int
+    var count: Int
+}
+
+class VendingMachine {
+    var inventory = [
+        "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    
+    
+    var coinsDeposited = 0
+    
+    
+    // throwing function
+    func vend(itemNamed name: String) throws {
+        guard let item = inventory[name] else {
+            throw VendingMachineError.invalidSelection
+        }
+        
+        guard item.count > 0 else {
+            throw VendingMachineError.outOfStock
+        }
+        
+        guard item.price <= coinsDeposited else {
+            throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinsDeposited)
+        }
+        
+        coinsDeposited -= item.price
+        
+        var newItem: Item = item
+        
+        newItem.count -= 1
+        
+        inventory[name] = newItem
+        
+        print("dispensing \(item)")
+    }
+}
