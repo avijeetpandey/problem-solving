b54b8985fe4d8bc27189f3539e2e9596dbe93292
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-29 00:28:04 +0530

    Add: GCD ques

--- problem-solving-swift/main.swift
+++ problem-solving-swift/main.swift
@@ -7,3 +7,23 @@
 
 import Foundation
 
+var counter = 1
+
+
+DispatchQueue.main.async {
+    for i in 0...3 {
+        counter = i
+        print("\(counter)")
+    }
+}
+
+for i in 4...6 {
+    counter = i
+    print("\(counter)")
+}
+
+
+DispatchQueue.main.async {
+    counter = 9
+    print(counter)
+}
