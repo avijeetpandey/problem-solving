9aa9cc6ba18682eaa0235fb0d4b43f6aa5dc6517
Avijeet pandey <avijeetpandey25@gmail.com>
2024-04-05 17:34:53 +0530

    Add: map , reduce functions

--- problem-solving-swift/swift/Hoc.swift
+++ problem-solving-swift/swift/Hoc.swift
@@ -11,6 +11,7 @@ import Foundation
 // sorted - sorts and return a new array , the original one remains unchanged
 // one more thing could be passed is if we want decresing > if increasing <
 
+// MARK: - demonstrates the usage of sorted function
 func sortedTest() {
     let numbers: [Int] = [1,20,0,89,61,4]
     let descendingNumbers = numbers.sorted { (a,b) in
@@ -32,4 +33,33 @@ func sortedTest() {
     let anotherSortedAnimals = animals.sorted(by: <)
     
     print(anotherSortedAnimals)
+}
+
+
+// MARK: - demonstrates usage of map function
+func mapTest() {
+    let numbers: [Int] = [1,2,3,4,5,6]
+    
+    let squaredNumbers = numbers.map { number in
+        return number*number
+    }
+    
+    print(squaredNumbers)
+    
+    // another way of writting the same
+    let doubledNumbers = numbers.map { $0 * 2 }
+    
+    print(doubledNumbers)
+}
+
+
+// MARK: - demonstrates the usage of reduce
+func reduceTest() {
+    let numbers: [Int] = [1,2,3,4]
+    
+    let sum = numbers.reduce(0) { partialResult, number in
+        return partialResult + number
+    }
+    
+    print(sum)
 }
