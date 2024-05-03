c967e8c4777c1fd30c0aab85e19228a12b6279b7
avijeet <avijeetpandey25@gmail.com>
2024-05-03 14:46:32 +0530

    Add: function return with closure

--- problem-solving-swift/swift/Closures.swift
+++ problem-solving-swift/swift/Closures.swift
@@ -10,6 +10,7 @@ import Foundation
 // MARK: - file to demonstrate usage of closures
 
 typealias ArithemeticClosure = (Int, Int) -> Int
+typealias MultiplierClosure = (Int) -> Int
 
 // defining closures
 let addClosure: (Int,Int) -> Int = { $0 + $1 }
@@ -21,11 +22,23 @@ func performOperation(on a: Int, and b: Int, operation: ArithemeticClosure) -> I
     return operation(a, b)
 }
 
+// function that returns a closure
+func makeMultiplier(with factor: Int) -> MultiplierClosure {
+    return { number in
+        return number * factor
+    }
+}
+
 func testClousres() {
     let addResult = performOperation(on: 5, and: 20, operation: addClosure)
     let multiplyResult = performOperation(on: 10, and: 10, operation: multiplyClosure)
     
     print(addResult)
     print(multiplyResult)
+    
+    let double = makeMultiplier(with: 2)
+    let doubleResult = double(10)
+    
+    print(doubleResult)
 }
 
