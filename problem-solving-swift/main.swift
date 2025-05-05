f2c4ed0f8b94a38ff13592a05ccde164f2270521
avijeetpandey <avijeetpandey25@gmail.com>
2025-05-05 21:13:52 +0530

    add: integer graph implementation

--- problem-solving-swift/main.swift
+++ problem-solving-swift/main.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+// Graph protocol
+protocol Graphable {
+    var adjacencyList: [[Int]] { get }
+    var graphSize: Int {get set}
+    func addDirectedEdge(from u: Int, to v: Int)
+    func addUndirectedEdge(from u: Int, to v: Int)
+}
+
+
+// Graph Implementation for integer data type
+class IntegerGraphImplementation: Graphable {
+    var adjacencyList: [[Int]]
+    var graphSize: Int
+    
+    // MARK: - init
+    init(size: Int) {
+        self.graphSize = size
+        self.adjacencyList = Array(repeating: [], count: size)
+    }
+    
+    func addDirectedEdge(from u: Int, to v: Int) {
+        guard v < graphSize , u < graphSize else { return }
+        adjacencyList[u].append(v)
+    }
+    
+    func addUndirectedEdge(from u: Int, to v: Int) {
+        guard v < graphSize , u < graphSize else { return }
+        adjacencyList[u].append(v)
+        adjacencyList[v].append(u) // make bidirectional connection with the nodes
+    }
+}
